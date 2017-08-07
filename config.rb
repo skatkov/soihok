88f1c0a63af3060ee2339b45b7c90b6e49a82399
Stanislav K <lunaticman@gmail.com>
2017-08-08 01:44:18 +0700

    adding deployment through middleman_s3-sync

--- config.rb
+++ config.rb
@@ -17,6 +17,24 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'www.soihok.com' # The name of the S3 bucket you are targeting. This is globally unique.
+  s3_sync.region                     = 'ap-southeast-1' # The AWS region for your bucket.
+  s3_sync.aws_access_key_id          = ENV['AWS_ACCESS_KEY']
+  s3_sync.aws_secret_access_key      = ENV['AWS_ACCESS_SECRET']
+  s3_sync.delete                     = false # We delete stray files by default.
+  s3_sync.after_build                = false # We do not chain after the build step by default.
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.prefix                     = ''
+  s3_sync.version_bucket             = false
+  s3_sync.index_document             = 'index.html'
+  s3_sync.error_document             = '404.html'
+end
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
